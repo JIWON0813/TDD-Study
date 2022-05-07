3f708f87a241d69608148cc4564ed62631745cac
JIWON0813 <59535499+JIWON0813@users.noreply.github.com>
2022-05-07 21:22:14 +0900

    feat 1장 실습

---
+++ src/test/ScoreCollectionTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScoreCollectionTest {
+
+    @Test
+    public void test(){
+        fail("Not yet implemented");
+    }
+}
